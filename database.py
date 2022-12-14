be0489e52f903713f41ecc5a0a4ee943b90ae3c6
vika <755128f@gmail.com>
2022-12-14 19:41:48 +0500

    2

--- database.py
+++ database.py
@@ -1,27 +1,28 @@
 import sqlite3
 
+
 # подключение и соединение с базой данных
 connect = sqlite3.connect('database.db', check_same_thread=False)
 cursor = connect.cursor()
 
+def create_db():
+    # создание таблиц
 
-# создание таблиц
+    cursor.execute('''CREATE TABLE IF NOT EXISTS "users" (
+        "id"	INTEGER NOT NULL,
+        "login"	TEXT NOT NULL UNIQUE,
+        "password"	TEXT NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );''')
+    connect.commit()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS "users" (
-	"id"	INTEGER NOT NULL,
-	"login"	TEXT NOT NULL UNIQUE,
-	"password"	TEXT NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);''')
-connect.commit()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS "links" (
-	"id"	INTEGER NOT NULL,
-	"link"	TEXT NOT NULL UNIQUE,
-	"short_link"	TEXT NOT NULL,
-	"access"	INTEGER NOT NULL,
-	"redirect_count"	INTEGER NOT NULL DEFAULT 0,
-	"user_id"	INTEGER NOT NULL,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);''')
-connect.commit()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS "links" (
+        "id"	INTEGER NOT NULL,
+        "link"	TEXT NOT NULL UNIQUE,
+        "short_link"	TEXT NOT NULL,
+        "access"	INTEGER NOT NULL,
+        "redirect_count"	INTEGER NOT NULL DEFAULT 0,
+        "user_id"	INTEGER NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );''')
+    connect.commit()
